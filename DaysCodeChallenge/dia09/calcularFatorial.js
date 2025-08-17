4d0553ab32f4ecb8f277990b3fb887f9a530542e
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-08-17 11:45:21 -0300

    Day 9 challenges

---
+++ DaysCodeChallenge/dia09/calcularFatorial.js
@@ -0,0 +1,21 @@
+import { readline } from "../../utils/readline.js";
+
+function perguntarNumero() {
+  readline.question(
+    "\nQual número você deseja calcular o fatorial? > ",
+    (numero) => {
+      calcularFatorial(numero);
+      readline.close();
+    }
+  );
+}
+
+function calcularFatorial(numero) {
+  let resultado = 1;
+  for (let i = 2; i <= numero; i++) {
+    resultado *= i;
+  }
+  console.log(`O fatorial de ${numero} é ${resultado}.\n`);
+}
+
+perguntarNumero();
