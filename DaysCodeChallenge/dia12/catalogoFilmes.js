fb22383f402d67ced44bc85a6ddf60814c3cee8a
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-08-20 14:26:22 -0300

    Day 12 challenges - initial setup

---
+++ DaysCodeChallenge/dia12/catalogoFilmes.js
@@ -0,0 +1,95 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const filmes = require("./db.json");
+
+import { readline } from "../../utils/readline.js";
+
+console.log("\nBem-vindo ao seu catálogo de filmes!");
+
+function exibirOpcoes() {
+  console.log("\nOpções da lista: \n ");
+  console.log("1 - Listar todos os filmes");
+  console.log("2 - Adicionar novo filme");
+  console.log("3 - Favoritar/Desfavoritar filme");
+  console.log("4 - Exibir filmes favoritos");
+  console.log("5 - Remover filme");
+  console.log("6 - Sair \n");
+  perguntarOpcao();
+}
+
+function perguntarOpcao() {
+  readline.question("Qual opção deseja? > ", (opcao) => {
+    if (opcao.toLowerCase() === "sair") {
+      opcao = 6;
+    } else {
+      opcao = Number(opcao);
+    }
+    verificarOpcao(opcao);
+  });
+}
+
+function verificarOpcao(opcao) {
+  switch (opcao) {
+    case 1:
+      exibirTodosOsFilmes();
+      readline.close();
+      break;
+    case 2:
+      adicionarNovoFilme();
+      readline.close();
+      break;
+    case 3:
+      alternarFavorito();
+      readline.close();
+      break;
+    case 4:
+      exibirFavoritos();
+      readline.close();
+      break;
+    case 5:
+      removerFilme();
+      readline.close();
+      break;
+    case 6:
+      console.log("\nEncerrando o programa...\n");
+      readline.close();
+      break;
+    default:
+      console.log("\nPor favor digite uma das seis opções válidas.\n");
+      perguntarOpcao();
+  }
+}
+
+function exibirTodosOsFilmes() {
+  console.log("\n=== Catálogo de Filmes ===\n");
+  filmes.map((filme) =>
+    filme.favorite === true
+      ? console.log(`${filme.id} - ${filme.title} (${filme.year}) ⭐`)
+      : console.log(`${filme.id} - ${filme.title} (${filme.year})`)
+  );
+  console.log("\n==========================\n");
+}
+
+function adicionarNovoFilme() {
+  console.log("Implementar função de adicionar filme.");
+}
+
+function alternarFavorito() {
+  console.log("Implementar função de alternar favorito.");
+}
+
+function exibirFavoritos() {
+  console.log("\n=== Filmes Favoritos ===\n");
+  filmes.map((filme) =>
+    filme.favorite === true
+      ? console.log(`${filme.id} - ${filme.title} (${filme.year}) ⭐`)
+      : ""
+  );
+  console.log("\n=========================\n");
+}
+
+function removerFilme() {
+  console.log("Implementar função de remover filme.");
+}
+
+exibirOpcoes();
